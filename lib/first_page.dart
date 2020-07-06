ec0fb1d2209e9d17f4a02432092963ac70fbb719
Fonkam Loïc <fonkamloic@gmail.com>
2020-07-06 12:48:21 +0100

    lib/first_page

---
+++ lib/first_page.dart
@@ -0,0 +1,23 @@
+import 'package:custom_page_transitions_tutorial/second_page.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class FirstPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.of(context).push(
+            PageRouteBuilder(
+              pageBuilder: (context, animation, secondaryAnimation) =>
+                  ListenableProvider(create: (context) => animation, child: SecondPage()),
+              transitionDuration: const Duration(seconds: 1),
+            ),
+          );
+        },
+        child: Icon(Icons.keyboard_arrow_right),
+      ),
+    );
+  }
+}
